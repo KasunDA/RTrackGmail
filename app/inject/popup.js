ca0bf3bc566ba1ad1af5ab8824044dd27fb95724
timjenks <timjenks@gmail.com>
2016-10-10 15:08:23 -0700

    Options localStorage working

--- app/inject/popup.js
+++ app/inject/popup.js
@@ -2,28 +2,25 @@
 
 console.log('popup.js');
 
-// function initSwitches() {
-// 		$("[name='cb-ui']").bootstrapSwitch();
-// 		$("[name='cb-default']").bootstrapSwitch();
-// 		$("[name='cb-notifications']").bootstrapSwitch();
-// 		$.fn.bootstrapSwitch.defaults.size = 'mini';
-// }
-
-// initSwitches();
-// $("[name='cb-ui']").bootstrapSwitch();
-// $("[name='cb-default']").bootstrapSwitch();
-// $("[name='cb-notifications']").bootstrapSwitch();
-// $.fn.bootstrapSwitch.defaults.size = 'mini';
-
 $(function() {
 	RTRACK_OPTIONS.initSwitches();
 	RTRACK_OPTIONS.initSwitchListeners();
 	RTRACK_OPTIONS.initSettings();
+
+	console.log('ui:', RTRACK_OPTIONS.settings.ui);
+	console.log('defaultState:', RTRACK_OPTIONS.settings.defaultState);
+	console.log('notifications:', RTRACK_OPTIONS.settings.notifications);
 });
 
 var RTRACK_OPTIONS = function() {
 
-		// Initialize boostrap-switch
+	var settings = {
+		ui: true,
+		defaultState: false,
+		notifications: true 
+	}
+
+	// Initialize boostrap-switch
 	function initSwitches() {
 		$("[name='cb-ui']").bootstrapSwitch();
 		$("[name='cb-default']").bootstrapSwitch();
@@ -36,50 +33,59 @@ var RTRACK_OPTIONS = function() {
 		$('input[name="cb-ui"]').on('switchChange.bootstrapSwitch', function(event, state) {
 		  console.log('UI switch changed to:', state); // true | false
 		  localStorage.setItem('ui', state.toString());
+		  settings.ui = state;
+		  console.log('ui:', settings.ui);
 		});
 
 		$('input[name="cb-default"]').on('switchChange.bootstrapSwitch', function(event, state) {
 		  console.log('Default switch changed to:', state); // true | false
 		  localStorage.setItem('defaultState', state.toString());
+		  settings.defaultState = state;
+		  console.log('defaultState:', settings.defaultState);
 		});
 
 		$('input[name="cb-notifications"]').on('switchChange.bootstrapSwitch', function(event, state) {
 		  console.log('Notification switch changed to:', state); // true | false
 		  localStorage.setItem('notifications', state.toString());
+		  settings.notifications = state;
+		  console.log('notifications:', settings.notifications);
 		});
 	}
 
-	// Read settings from localStorage
 	function initSettings() {
-		// Defaults
-		var ui = true;
-		var defaultState = false;
-		var notifications = true;
 
 		var storage = {};
 		storage.ui = localStorage.getItem('ui');
 		storage.defaultState = localStorage.getItem('defaultState');
 		storage.notifications = localStorage.getItem('notifications');
 
-		if (storage.ui != null) {
-			ui = !!storage.ui;
+		// Use localStorage settings or defaults
+		if (storage.ui == null||undefined) {
+			localStorage.setItem('ui', settings.ui.toString());
+		} else {
+			this.settings.ui = (storage.ui == 'true');
 		}
 
-		if (storage.defaultState != null) {
-			defaultState = !!storage.defaultState;
+		if (storage.defaultState == null||undefined ) {
+			localStorage.setItem('defaultState', settings.defaultState.toString());
+		} else {
+			this.settings.defaultState = (storage.defaultState == 'true');
 		}
 
-		if (storage.notifications != null) {
-			notifications = !!storage.notifications;
+		if (storage.notifications == null||undefined) {
+			localStorage.setItem('notifications', settings.notifications.toString());
+		} else {
+			this.settings.notifications = (storage.notifications == 'true');
 		}
 		
-		$('[name="cb-ui"]').bootstrapSwitch('state', ui, true);
-		$('[name="cb-default"]').bootstrapSwitch('state', defaultState, true);
-		$('[name="cb-notifications"]').bootstrapSwitch('state', notifications, true);
+		// Set switch state based on settings
+		$('[name="cb-ui"]').bootstrapSwitch('state', this.settings.ui, false);
+		$('[name="cb-default"]').bootstrapSwitch('state', this.settings.defaultState, false);
+		$('[name="cb-notifications"]').bootstrapSwitch('state', this.settings.notifications, false);
 	}
 	
-
 	return {
+		settings: settings,
 		initSwitches: initSwitches,
 		initSwitchListeners: initSwitchListeners,
 		initSettings: initSettings
