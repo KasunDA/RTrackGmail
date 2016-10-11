d0ef9c1247f48d67ca7168f33015d2bb164477b0
timjenks <timjenks@gmail.com>
2016-10-10 20:52:27 -0700

    Chrome storage implemented in settings popup

--- app/inject/popup.js
+++ app/inject/popup.js
@@ -5,15 +5,18 @@ console.log('popup.js');
 $(function() {
 	RTRACK_OPTIONS.initSwitches();
 	RTRACK_OPTIONS.initSwitchListeners();
-	RTRACK_OPTIONS.initSettings();
+	RTRACK_OPTIONS.initSettings(function(err, data) {
+		if (err) {
+			return console.log('Error Initializing settings');
+		}
 
-	console.log('ui:', RTRACK_OPTIONS.settings.ui);
-	console.log('defaultState:', RTRACK_OPTIONS.settings.defaultState);
-	console.log('notifications:', RTRACK_OPTIONS.settings.notifications);
+		console.log('Initialized settings:', data);
+	});
 });
 
 var RTRACK_OPTIONS = function() {
 
+	// Defaults
 	var settings = {
 		ui: true,
 		defaultState: false,
@@ -22,68 +25,94 @@ var RTRACK_OPTIONS = function() {
 
 	// Initialize boostrap-switch
 	function initSwitches() {
+		console.log('this.settings:', this.settings);
 		$("[name='cb-ui']").bootstrapSwitch();
 		$("[name='cb-default']").bootstrapSwitch();
 		$("[name='cb-notifications']").bootstrapSwitch();
+		$("[name='cb-ui']").bootstrapSwitch('state', this.settings.ui, true);
+		$("[name='cb-default']").bootstrapSwitch('state', this.settings.defaultState, true);
+		$("[name='cb-notifications']").bootstrapSwitch('state', this.settings.notifications, true);
 		$.fn.bootstrapSwitch.defaults.size = 'mini';
 	}
 
 	// Event handlers to update stored settings on switch changes
 	function initSwitchListeners() {
 		$('input[name="cb-ui"]').on('switchChange.bootstrapSwitch', function(event, state) {
-		  console.log('UI switch changed to:', state); // true | false
-		  localStorage.setItem('ui', state.toString());
-		  settings.ui = state;
-		  console.log('ui:', settings.ui);
+		  console.log('UI switch changed to:', state);
+		  chrome.storage.sync.set({'ui': state}, function() {
+		  	console.log('ui setting saved:', state);
+		  });
 		});
 
 		$('input[name="cb-default"]').on('switchChange.bootstrapSwitch', function(event, state) {
-		  console.log('Default switch changed to:', state); // true | false
-		  localStorage.setItem('defaultState', state.toString());
-		  settings.defaultState = state;
-		  console.log('defaultState:', settings.defaultState);
+		  console.log('Default state switch changed to:', state);
+		  chrome.storage.sync.set({'defaultState': state}, function() {
+		  	console.log('defaultState setting saved:', state);
+		  });
 		});
 
 		$('input[name="cb-notifications"]').on('switchChange.bootstrapSwitch', function(event, state) {
-		  console.log('Notification switch changed to:', state); // true | false
-		  localStorage.setItem('notifications', state.toString());
-		  settings.notifications = state;
-		  console.log('notifications:', settings.notifications);
+		  console.log('Notifications switch changed to:', state);
+		  chrome.storage.sync.set({'notifications': state}, function() {
+		  	console.log('notifications setting saved:', state);
+		  });
 		});
 	}
 
-	function initSettings() {
+	// Initialize settings
+	function initSettings(callback) {
 
-		var storage = {};
-		storage.ui = localStorage.getItem('ui');
-		storage.defaultState = localStorage.getItem('defaultState');
-		storage.notifications = localStorage.getItem('notifications');
-
-		// Use localStorage settings or defaults
-		if (storage.ui == null||undefined) {
-			localStorage.setItem('ui', settings.ui.toString());
-		} else {
-			this.settings.ui = (storage.ui == 'true');
-		}
-
-		if (storage.defaultState == null||undefined ) {
-			localStorage.setItem('defaultState', settings.defaultState.toString());
-		} else {
-			this.settings.defaultState = (storage.defaultState == 'true');
-		}
-
-		if (storage.notifications == null||undefined) {
-			localStorage.setItem('notifications', settings.notifications.toString());
-		} else {
-			this.settings.notifications = (storage.notifications == 'true');
-		}
-		
-		// Set switch state based on settings
-		$('[name="cb-ui"]').bootstrapSwitch('state', this.settings.ui, false);
-		$('[name="cb-default"]').bootstrapSwitch('state', this.settings.defaultState, false);
-		$('[name="cb-notifications"]').bootstrapSwitch('state', this.settings.notifications, false);
+		initStorage(this.settings, function(err, data) {
+			if (err) {
+				return callback(err);
+			}
+			callback(null, data);
+		});
 	}
-	
+
+	// Update settings and set switch state
+	function initStorage(settings, callback) {
+		var config = {
+			ui: {key: 'ui', defaultVal: settings.ui},
+			defaultState: {key: 'defaultState', defaultVal: settings.defaultState},
+			notifications: {key: 'notifications', defaultVal: settings.notifications}
+		}
+
+    async.parallel({
+        ui: initStorageSetting.bind(null, config.ui),
+        defaultState: initStorageSetting.bind(null, config.defaultState),
+       	notifications: initStorageSetting.bind(null, config.notifications)
+    }, function(err, data) {
+        if (err) {
+            return callback(err);
+        }
+
+				// Set switch state based on settings
+				$('[name="cb-ui"]').bootstrapSwitch('state', data.ui, true);
+				$('[name="cb-default"]').bootstrapSwitch('state', data.defaultState, true);
+				$('[name="cb-notifications"]').bootstrapSwitch('state', data.notifications, true);
+
+        callback(null, data);
+	  });
+  }
+
+  // Retrieve setting from chrome storage or update chrome storage with default setting
+	function initStorageSetting(setting, callback) { 
+		chrome.storage.sync.get(setting.key, function(data) {
+			if (data[setting.key] == null||undefined) {
+				var chromeSetting = {};
+				chromeSetting[setting.key] = setting.defaultVal;
+				chrome.storage.sync.set(chromeSetting, function() {
+					console.log('Saved setting: ' + setting.key + ': ' + setting.defaultVal);
+					callback(null, setting.defaultVal);
+				});
+			} else {
+				console.log('Retrieved setting ' + setting.key + ': ' + data[setting.key]);
+				callback(null, data[setting.key]);
+			}
+		});
+	}
+
 	return {
 		settings: settings,
 		initSwitches: initSwitches,
